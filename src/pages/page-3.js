673f67a3170ff8987c30a0b9118804bef353065d
arunsnd2017 <arunsinghsnd@gmail.com>
2020-07-07 23:22:26 +0530

    adding pages for testing

---
+++ src/pages/page-3.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+
+
+const ThirdPage = () => (
+  <Layout>
+    <h1>Hello form the thirdPage</h1>
+  </Layout>
+)
+
+export default  ThirdPage
